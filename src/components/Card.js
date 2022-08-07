c66609b0cd536cca78538ba4b3090cdcd8c46a8e
solch4 <sol.maldonado.0209@gmail.com>
2022-08-07 13:04:13 -0300

    card link updated

--- src/components/Card.js
+++ src/components/Card.js
@@ -16,27 +16,25 @@ export default function Card({
       <button className="closeBtn" onClick={onClose}>
         X
       </button>
-
-      <Link className="card-title" to={`/city/${id}`}>
+      <Link to={`/city/${id}`}>
         <h5 className="card-title">{name}</h5>
+        <div className="card-body">
+          <div className="card-temp">
+            <p className="temp">Temp {temp}°</p>
+            <p className="feels_like">Feels like {feels_like}°</p>
+          </div> {/* card-temp */}
+          <div className="card-weather">
+            <img
+              className="icon"
+              src={"http://openweathermap.org/img/wn/" + img + "@2x.png"}
+              width="80"
+              height="80"
+              alt=""
+            />
+            <p className="weatherMain">{weatherMain}</p>
+          </div> {/* card-weather */}
+        </div> {/* card-body */}
       </Link>
-
-      <div className="card-body">
-        <div className="card-temp">
-          <p className="temp">Temp {temp}°</p>
-          <p className="feels_like">Feels like {feels_like}°</p>
-        </div>{/* card-temp */}
-        <div className="card-weather">
-          <img
-            className="icon"
-            src={"http://openweathermap.org/img/wn/" + img + "@2x.png"}
-            width="80"
-            height="80"
-            alt=""
-          />
-          <p className="weatherMain">{weatherMain}</p>
-        </div>{/* card-temp */}
-      </div>{/* card-body */}
     </div>
   );
 }
